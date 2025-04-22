4ab49943dcf1c62467b2a3d3fbe772b95a892d1a
Victor Pedroso <p.victor@cicclo.com>
2025-04-22 11:05:22 -0300

    update endpoint aws

--- src/apis/client/index.ts
+++ src/apis/client/index.ts
@@ -4,7 +4,6 @@ export const useClienteApi = () => {
   const api = new Api({
     baseURL: 'https://ecjirdsy35iqhfqjtxbuf5zzda0euegm.lambda-url.us-east-1.on.aws',
     withCredentials: true,
-    secure: true,
   });
 
   return api.api;
